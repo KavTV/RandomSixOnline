59e9f505ff0e91825c02a08d694f9a5150c354b9
KavTV <46051618+KavTV@users.noreply.github.com>
2021-08-01 15:29:46 +0200

    Initial commit

---
+++ CommunicationHub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace RandomSixOnline
+{
+    public class CommunicationHub : Hub
+    {
+
+        public async Task SendPlayer(Player player, string groupName)
+        {
+            await Clients.Group(groupName).SendAsync("GetPlayer", player);
+        }
+        public Task JoinRoom(string roomName)
+        {
+            return Groups.AddToGroupAsync(this.Context.ConnectionId, roomName);
+            //Leave a message or add html with player joined.
+        }
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public Task LeaveRoom(string roomName)
+        {
+            return Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        public Task SendMesssageToGroup(string group, string message)
+        {
+            return Clients.Group(group).SendAsync(message);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RandomSixOnline
+{
+    public class Player
+    {
+        public string Username { get => username; set => username = value; }
+        public string ConnectionId { get => connectionId; set => connectionId = value; }
+        public string CurrentCharacter { get => currentCharacter; set => currentCharacter = value; }
+
+        private string username;
+        private string connectionId;
+        private string currentCharacter;
+
+        public Player(){ }
+        public Player(string currentCharacter)
+        {
+            this.currentCharacter = currentCharacter;
+        }
+
+        public Player(string username, string connectionId, string currentCharacter)
+        {
+            this.username = username;
+            this.connectionId = connectionId;
+            this.currentCharacter = currentCharacter;
+        }
+
+        string GetRandomOperator()
+        {
+            //ALL operators available
+            string[] operators = new string[] {"Thunderbird","Flores", "Aruni", "Zero", "Ace", "Melusi", "Oryx",
+            "Iana","Wamai","Kali","Amaru","Goyo","Nøkk","Warden","Mozzie","Gridlock","Nomad","Kaid",
+            "Clash","Maverick","Maestro","Alibi","Lion","Finka","Vigil","Dokkaebi","Zofia","Ela","Ying",
+            "Lesion","Mira","Jackal","Hibana","Echo","Caveira","Capitao","Blackbeard","Vallkyrie","Buck","Frost",
+            "Mute","Sledge","Smoke","Ash","Castle","Pulse","Thermite","Montagne","Twitch","Doc","Rook",
+            "Jäger","Bandit","Blitz","Iq","Fuze","Glaz","Tachanka","Kapkan"};
+            Random rnd = new Random();
+
+            string randomOperator = operators[rnd.Next(0, operators.Length)];
+            this.currentCharacter = randomOperator;
+
+            return randomOperator;
+        }
+    }
+}
